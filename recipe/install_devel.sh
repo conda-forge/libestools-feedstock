087fb96d08ee850ca75896f1af5e537b84b9c92e
Martin Pecka <peckama2@fel.cvut.cz>
2023-02-07 04:56:52 +0100

    Tuning the content of -dev package

--- recipe/install_devel.sh
+++ recipe/install_devel.sh
@@ -1,5 +1,20 @@
 set -exou
 
+# user_flags.mak is used by downstream packages to configure their build, but we do not want to enforce CC or CXX
+sed -i '/^CC =/d' config/user_flags.mak
+sed -i '/^CXX =/d' config/user_flags.mak
+
+# borrowed from Arch linux - substitute #include "EST.h" for #include <speech_tools/EST.h> to ease finding the headers
+for i in $(find . -type f -a \( -name '*.h' -o -name '*.cc' -o -name '*.c' -o -name '*.cpp' \) ); do
+  sed -i -e 's,"EST.*\.h",\<speech_tools/&\>,g' -e 's,speech_tools/\",speech_tools/,g' \
+      -e 's,"siod.*\.h",\<speech_tools/&\>,g' -e 's,speech_tools/\",speech_tools/,g' \
+      -e 's,"instantiate/.*\.h",\<speech_tools/&\>,g' -e 's,speech_tools/instantiate/\",speech_tools/instantiate/,g' -e 's,"instantiate,instantiate,g' \
+      -e 's,"ling_class/.*\.h",\<speech_tools/&\>,g' -e 's,speech_tools/ling_class/\",speech_tools/ling_class/,g' -e 's,"ling_class,ling_class,g' \
+      -e 's,"rxp/.*\.h",\<speech_tools/&\>,g' -e 's,speech_tools/rxp/\",speech_tools/rxp/,g' -e 's,"rxp,rxp,g' \
+      -e 's,"sigpr/.*\.h",\<speech_tools/&\>,g' -e 's,speech_tools/sigpr/\",speech_tools/sigpr/,g' -e 's,"sigpr,sigpr,g' \
+      -e 's,"unix/.*\.h",\<speech_tools/&\>,g' -e 's,speech_tools/unix/\",speech_tools/unix/,g' -e 's,\.h\">,.h\>,g' -e 's,"unix,unix,g' $i
+done
+
 mkdir -p "${PREFIX}/include/speech_tools"
 mkdir -p "${PREFIX}/include/speech_tools/unix"
 mkdir -p "${PREFIX}/include/speech_tools/base_class"
