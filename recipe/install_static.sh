5bf9c382fdc6c87c672cbc058a783dc4f10e0453
Martin Pecka <peci1@seznam.cz>
2023-07-25 10:29:03 +0200

    Added libestools-static package

---
+++ recipe/install_static.sh
@@ -0,0 +1,8 @@
+set -exou
+
+cp lib/lib*.a "${PREFIX}/lib/"
+
+# Some parts of the libraries may need these crazy links...
+ln -s "${PREFIX}/lib/libestbase.a"     "${PREFIX}/lib/speech_tools/lib/libestbase.a"
+ln -s "${PREFIX}/lib/libeststring.a"   "${PREFIX}/lib/speech_tools/lib/libeststring.a"
+ln -s "${PREFIX}/lib/libestools.a"     "${PREFIX}/lib/speech_tools/lib/libestools.a"
